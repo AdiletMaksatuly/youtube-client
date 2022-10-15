0d5380180a29b0baa54cf051fdd510886ae2dcef
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-10-15 21:53:09 +0600

    feat: implement login page layout

--- youtube-client/src/app/auth/auth.module.ts
+++ youtube-client/src/app/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   exports: [],
 })
 export class AuthModule {}
