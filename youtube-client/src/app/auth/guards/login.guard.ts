88d2d95be862622052745acb6937a4a7b4dcbae3
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-10-22 12:13:03 +0600

    refactor: delete unused types from LoginGuard's canLoad return

--- youtube-client/src/app/auth/guards/login.guard.ts
+++ youtube-client/src/app/auth/guards/login.guard.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[],
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree {
     const fullURL = segments.map((segment) => segment.path).join('/');
 
     return this.checkLogin(fullURL);
