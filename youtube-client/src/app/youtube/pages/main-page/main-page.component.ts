ffb5c3bcc97f83db29bd16e7e9a8b6a486bb203d
Aliaksandr_Akhremchyk <A.Akhremhcyk@wolterskluwer.com>
2022-10-22 12:21:10 +0200

    refactor: remove subscribe in MainPageComponent

--- youtube-client/src/app/youtube/pages/main-page/main-page.component.ts
+++ youtube-client/src/app/youtube/pages/main-page/main-page.component.ts
@@ -1,45 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SearchService } from '../../../core/services/search.service';
 import { FilterType } from '../../../core/models/filter.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements OnInit {
-  searchQuery: string | null = null;
+export class MainPageComponent {
+  searchQuery$: Observable<string | null> = this.youtubeService.getSearchQuery();
 
-  filterQuery: FilterType = null;
+  filterQuery$: Observable<FilterType> = this.youtubeService.getFilterQuery();
 
-  filterString: string = '';
+  filterString$: Observable<string> = this.youtubeService.getFilterString();
 
   constructor(private youtubeService: SearchService) {}
-
-  ngOnInit(): void {
-    this.getSearchQuery();
-    this.getFilterQuery();
-    this.getFilterString();
-  }
-
-  getSearchQuery(): void {
-    this.youtubeService.getSearchQuery().subscribe((searchQuery) => {
-      this.searchQuery = searchQuery;
-      return this.searchQuery;
-    });
-  }
-
-  getFilterQuery(): void {
-    this.youtubeService.getFilterQuery().subscribe((filterQuery) => {
-      this.filterQuery = filterQuery;
-      return this.filterQuery;
-    });
-  }
-
-  getFilterString(): void {
-    this.youtubeService.getFilterString().subscribe((filterString) => {
-      this.filterString = filterString;
-      return this.filterString;
-    });
-  }
 }
