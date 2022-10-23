b3445d875014a7fc7b6c5fc2551e47e2096c9810
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-10-23 18:30:36 +0600

    feat: add error catching

--- youtube-client/src/app/youtube/services/youtube.service.ts
+++ youtube-client/src/app/youtube/services/youtube.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { map, Observable, switchMap } from 'rxjs';
-import { SearchListResponse, Video, VideosListResponse } from '../models/video.model';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { API_KEY } from '../api-key.constant';
+import {Injectable} from '@angular/core';
+import {catchError, map, Observable, switchMap, throwError} from 'rxjs';
+import {SearchListResponse, Video, VideosListResponse} from '../models/video.model';
+import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
+import {API_KEY} from '../api-key.constant';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,8 @@ export class YoutubeService {
 
   private VIDEOS_URL = 'https://www.googleapis.com/youtube/v3/videos';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getVideoIDs(searchQuery: string): Observable<string> {
     const params = new HttpParams()
@@ -21,11 +22,13 @@ export class YoutubeService {
       .set('maxResults', 10)
       .set('q', searchQuery);
 
-    return this.http.get<SearchListResponse>(this.SEARCH_URL, { params }).pipe(
-      map((response) => {
+    return this.http.get<SearchListResponse>(this.SEARCH_URL, {params})
+      .pipe(
+        map((response) => {
         return response?.items.map((video) => video.id.videoId).join(',');
-      }),
-    );
+        }),
+        catchError(this.handleError)
+      );
   }
 
   getVideos(searchQuery: string): Observable<Video[]> {
@@ -37,18 +40,36 @@ export class YoutubeService {
             .set('part', 'snippet,statistics')
             .set('id', IDs);
 
-          return this.http.get<VideosListResponse>(this.VIDEOS_URL, { params });
+          return this.http.get<VideosListResponse>(this.VIDEOS_URL, {params});
         }),
       )
-      .pipe(map((response) => response?.items));
+      .pipe(
+        map((response) => response?.items),
+        catchError(this.handleError)
+      );
   }
 
   getVideo(id: string): Observable<Video> | null {
-    const params = new HttpParams();
-    params.set('key', API_KEY);
-    params.set('part', 'snippet,statistics');
-    params.set('id', id);
+    const params = new HttpParams()
+      .set('key', API_KEY)
+      .set('part', 'snippet,statistics')
+      .set('id', id);
 
-    return this.http.get<Video>(this.VIDEOS_URL, { params });
+    return this.http.get<VideosListResponse>(this.VIDEOS_URL, {params})
+      .pipe(
+        map(response => response.items[0]),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
