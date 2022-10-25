fdbd5a457f87931b762ebcb7725cea43c3d1bc46
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-10-25 23:07:36 +0600

    feat: add navigation to admin page on logo click

--- youtube-client/src/app/core/components/logo/logo.component.ts
+++ youtube-client/src/app/core/components/logo/logo.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {YoutubeRoutePaths} from "../../../youtube/models/routes.model";
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent {}
+export class LogoComponent {
+  private adminPageRoute = YoutubeRoutePaths.ADMIN_PAGE;
+
+  constructor(private router: Router) {
+  }
+  goToAdminPage() {
+    this.router.navigate(['main/' + this.adminPageRoute])
+  }
+}
