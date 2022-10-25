a256db5e015597b151d9fbce93a986bb955f2fd9
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-10-26 00:35:39 +0600

    refactor: delete twice used async pipe in template of detailed-information-page.component.html and remove unused api key const

--- youtube-client/src/app/youtube/models/api.model.ts
+++ youtube-client/src/app/youtube/models/api.model.ts
@@ -4,5 +4,4 @@ export enum URLs {
   VIDEOS_LIST = 'videos',
 }
 
-// export const API_KEY = 'AIzaSyB04AiQ-RaKwmBlfdFOw0lz5vAp_A6K6bo';
 export const API_KEY = 'AIzaSyC4QNrkrrdCJW7kGp7Gybw56dknottx9KY';
